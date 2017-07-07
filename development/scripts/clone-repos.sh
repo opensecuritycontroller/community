55950aa4f767bd9a8f89ae6f61f7253cc19bbb24
Arvind Nadendla <arvind.nadendla@intel.com>
2017-07-06 18:17:45 -0700

    remove prepush hooks for repositories
    remove prepush hooks for repositories because pushes to forks are not restricted.

--- development/scripts/clone-repos.sh
+++ development/scripts/clone-repos.sh
@@ -5,9 +5,7 @@ user=ENTER_USERNAME
 
 repos=(
 "osc-core" 
-#"security-mgr-smc-plugin"
 "security-mgr-sample-plugin"
-#"security-mgr-nsm-plugin"
 "security-mgr-api"
 "sdn-controller-api"
 "sdn-controller-nsc-plugin"
@@ -22,7 +20,5 @@ do
     cd "${repo}"/
     git remote add upstream git@github.com:opensecuritycontroller/"${repo}".git
     git remote set-url --push upstream no_push
-    cd ../"${repo}"/.git/hooks 
-    ln -s ../../hooks/pre-push .
-    cd ../../../
+    cd ../
 done
